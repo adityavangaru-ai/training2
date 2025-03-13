c10f49de97c5d3c856adc8545287fa769de1354a
adityavangaru-ai <adithya2002vangaru@gmail.com>
2025-03-13 17:51:43 +0530

    Add files via upload

---
+++ letVarandConst.js
@@ -0,0 +1,44 @@
+let firstName = 'Aditya'
+let lastName = 'Vangaru'
+
+console.log(firstName + ' ' + lastName);
+let age =22;
+let birthDate = new Date();
+
+let person = {
+    firstName : 'Shiva',
+    lastName : 'Prasad'
+}
+
+console.log(person)
+
+const department = 'Training'
+
+console.log(department)
+
+const customer = {
+    firstName : 'Shiva',
+    lastName : 'Prasad'
+}
+console.log(customer)
+
+const customer2 = {
+    firstName : 'Abshiek',
+    lastName : 'Sharma'
+}
+
+customer2.firstName = 'sudipta'
+console.log(customer2)
+
+let squareofNumber = function(value)
+{
+    return value*value;
+}
+
+console.log(squareofNumber(25));
+
+var index2 = 10;
+for (let index = 0; index <= 10; index++){
+    console.log(index)
+}
+console.log(index2)
